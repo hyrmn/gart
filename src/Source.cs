51d5e8d6fe85c24123e3b86f642d8767031e9df3
Ben Hyrman <ben@hyr.mn>
2021-09-23 23:49:26 -0400

    Improve Sources VO

--- src/Source.cs
+++ src/Source.cs
@@ -1,3 +1,12 @@
 ﻿namespace gart;
 
-public record Source(FileInfo[] Files);
+public record Source
+{
+    public Source(FileInfo[] files)
+    {
+        if (files.Any(file => !file.Exists)) throw new ArgumentException("All files used as sources must exist", nameof(files));
+        Files = files;
+    }
+
+    public FileInfo[] Files { get; }
+}
